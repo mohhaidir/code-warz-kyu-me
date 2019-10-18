e6fbc761178fb70ca816ebf079f0aea5e736bfbb
mohhaidir <khoiruljmbr@gmail.com>
2019-10-18 15:53:52 +0700

    fixed with new solution

--- SumOfTheFirstNthTermOfSeries.js
+++ SumOfTheFirstNthTermOfSeries.js
@@ -32,6 +32,13 @@ function SeriesSum(n) {
         pembagi += 3
     }
     return summary.toFixed(2)
+
+    /*
+    for (var s = 0, i = 0; i < n; i++) {
+        s += 1 / (1 + i * 3)
+    }
+    return s.toFixed(2)
+    */
 }
 
 console.log(SeriesSum(1), "1.00")

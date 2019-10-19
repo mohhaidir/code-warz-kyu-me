c67daa20cd9031a0f8b7075a74a1f480fe889709
mohhaidir <khoiruljmbr@gmail.com>
2019-10-20 02:17:35 +0700

    added another solution

--- RomansNumeralDecoder.js
+++ RomansNumeralDecoder.js
@@ -42,7 +42,33 @@ function solution(roman) {
     })
     return m + romanNumerals[roman[roman.length - 1]]
 }
+// <<<=== add new solution ===>>>
+/*
+function solution(roman) {
+    var data = {
+        M: 1000,
+        D: 500,
+        C: 100,
+        L: 50,
+        X: 10,
+        V: 5,
+        I: 1
+    }
+    var numbers = roman.split('')
+    var sum = 0,
+        i
 
+    for (i = 0; i < numbers.length; i++) {
+        if (data[numbers[i]] < data[numbers[i + 1]]) {
+            sum += data[numbers[i + 1]] - data[numbers[i]]
+            i++
+        } else {
+            sum += data[numbers[i]]
+        }
+    }
+    return sum
+}
+*/
 console.log(solution('XXI') == 21, 'XXI should == 21')
 console.log(solution('I') == 1, 'I should == 1')
 console.log(solution('IV') == 4, 'IV should == 4')

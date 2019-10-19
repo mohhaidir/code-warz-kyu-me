cab203b10dcece76e1ff19df56223c7385542a79
mohhaidir <khoiruljmbr@gmail.com>
2019-10-20 02:45:07 +0700

    added new solution

--- CamelCaseMethod.js
+++ CamelCaseMethod.js
@@ -33,6 +33,13 @@ function camelCase(str) {
 String.prototype.camelCase = function(){  
     return this.split(' ').map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join('')
 }
+Test.describe("Basic tests",_=>{
+Test.assertEquals("test case".camelCase(), "TestCase");
+Test.assertEquals("camel case method".camelCase(), "CamelCaseMethod");
+Test.assertEquals("say hello ".camelCase(), "SayHello");
+Test.assertEquals(" camel case word".camelCase(), "CamelCaseWord");
+Test.assertEquals("".camelCase(), "");
+})
 */
 console.log(camelCase("test case"), "TestCase");
 console.log(camelCase("camel case method"), "CamelCaseMethod");

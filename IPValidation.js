9f5289fef6a94075afd3c7d7be49ad4ed0ca59bd
mohhaidir <khoiruljmbr@gmail.com>
2019-10-19 20:44:23 +0700

    added new kata\s

--- IPValidation.js
+++ IPValidation.js
@@ -79,6 +79,10 @@ function isValidIP(str) {
     return /^(([1-9]?\d|1\d\d|2[0-4]\d|25[0-5])(\.(?!$)|$)){4}$/.test(str)
 }
 */
+/*
+const net = require('net');
+const isValidIP = (s) => net.isIP(s);
+*/
 console.log(isValidIP("0.0.0.0"), true);
 console.log(isValidIP("12.255.56.1"), true);
 console.log(isValidIP("137.255.156.100"), true);

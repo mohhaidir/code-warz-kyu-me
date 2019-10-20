dc38634a83ff621f15491b838915f50c945fa9ff
mohhaidir <khoiruljmbr@gmail.com>
2019-10-20 23:04:56 +0700

    fixed line 14

--- CountCharactersInYourString.js
+++ CountCharactersInYourString.js
@@ -11,11 +11,7 @@ function count(string) {
     let result = {}
     let str = string.split('').sort()
     for (let i = 0; i < str.length; i++) {
-        if (result[str[i]] == undefined) {
-            result[str[i]] = 1
-        } else {
-            result[str[i]]++
-        }
+        (result[str[i]] == undefined) ? result[str[i]] = 1: result[str[i]]++
     }
     return result
 }

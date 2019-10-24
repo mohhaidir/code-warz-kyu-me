53253facae02ef25a2732a4168cb760ea3d8b9e0
mohhaidir <khoiruljmbr@gmail.com>
2019-10-25 03:47:07 +0700

    add new solution

--- ValidPhoneNumber.js
+++ ValidPhoneNumber.js
@@ -38,5 +38,11 @@ function validPhoneNumber(phoneNumber) {
         }
     }
 }
+// <<<=== add new solution with regex ===>>>
+/*
+function validPhoneNumber(phoneNumber) {
+    return /^\(\d{3}\) \d{3}\-\d{4}$/.test(phoneNumber);
+}
+*/
 
 console.log(validPhoneNumber("(123) 456-7890"), true);

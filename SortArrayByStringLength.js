9dc16575be6d40d25a0c87a6d3574ed077b86c1b
mohhaidir <khoiruljmbr@gmail.com>
2019-10-25 07:17:19 +0700

    add new solution

--- SortArrayByStringLength.js
+++ SortArrayByStringLength.js
@@ -25,7 +25,12 @@ function sortByLength(array) {
     }
     return array
 }
-
+// <<<=== add new solution ===>>>
+/*
+function sortByLength(array) {
+    return array.sort((a, b) => a.length - b.length)
+}
+*/
 console.log(sortByLength(["Beg", "Life", "I", "To"]), ["I", "To", "Beg", "Life"]);
 console.log(sortByLength(["", "Moderately", "Brains", "Pizza"]), ["", "Pizza", "Brains", "Moderately"]);
 console.log(sortByLength(["Longer", "Longest", "Short"]), ["Short", "Longer", "Longest"]);

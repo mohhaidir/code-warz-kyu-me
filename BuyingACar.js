8d4d471aabb90ec8a1f7ca2bedf055253b667237
mohhaidir <khoiruljmbr@gmail.com>
2019-10-27 23:48:19 +0700

    add new kata\s

---
+++ BuyingACar.js
@@ -0,0 +1,52 @@
+/*
+Let us begin with an example:
+
+A man has a rather old car being worth $2000. He saw a secondhand car being worth $8000. He wants to keep his old car until he can buy the secondhand one.
+
+He thinks he can save $1000 each month but the prices of his old car and of the new one decrease of 1.5 percent per month. Furthermore this percent of loss increases by 0.5 percent at the end of every two months. Our man finds it difficult to make all these calculations.
+
+Can you help him?
+
+How many months will it take him to save up enough money to buy the car he wants, and how much money will he have left over?
+
+Parameters and return of function:
+
+parameter (positive int or float, guaranteed) startPriceOld (Old car price)
+parameter (positive int or float, guaranteed) startPriceNew (New car price)
+parameter (positive int or float, guaranteed) savingperMonth 
+parameter (positive float or int, guaranteed) percentLossByMonth
+
+nbMonths(2000, 8000, 1000, 1.5) should return [6, 766] or (6, 766)
+where 6 is the number of months at the end of which he can buy the new car and 766 is the nearest integer to 766.158 (rounding 766.158 gives 766).
+
+Note:
+
+Selling, buying and saving are normally done at end of month. Calculations are processed at the end of each considered month but if, by chance from the start, the value of the old car is bigger than the value of the new one or equal there is no saving to be made, no need to wait so he can at the beginning of the month buy the new car:
+
+nbMonths(12000, 8000, 1000, 1.5) should return [0, 4000]
+nbMonths(8000, 8000, 1000, 1.5) should return [0, 0]
+We don't take care of a deposit of savings in a bank:-)
+*/
+
+function nbMonths(startPriceOld, startPriceNew, savingperMonth, percentLossByMonth) {
+    let months = 0
+    let savingsTotal = 0
+    let lp = percentLossByMonth
+    let ocv = startPriceOld
+    let ncv = startPriceNew
+
+    while ((ocv + savingsTotal) < ncv) {
+        months++
+        months % 2 == 0 ? lp += (.5) : lp
+
+        ocv -= ocv * (lp / 100)
+        ncv -= ncv * (lp / 100)
+        savingsTotal += savingperMonth
+    }
+
+    let leftoverSavings = (ocv + savingsTotal) - ncv
+    return [months, Math.round(leftoverSavings)]
+}
+
+console.log(nbMonths(2000, 8000, 1000, 1.5), [6, 766])
+console.log(nbMonths(12000, 8000, 1000, 1.5), [0, 4000])
